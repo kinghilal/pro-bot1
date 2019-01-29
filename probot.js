f8ffb879452dcbcb7930b52d2e15720951b19fa9
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-01-29 16:22:34 +0300

    Update probot.js

--- probot.js
+++ probot.js
@@ -3,7 +3,7 @@ const moment = require("moment");
 const fs = require("fs");      
 const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
-const Canvas = require("canvas"); //npm i canvas
+const Canvas = require("canvas"); 
 const prefix = "+" 
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
