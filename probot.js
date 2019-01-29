3026cf17d6ca9a4572e0829ccf6f49c035aa5bfc
kinghilal <46085303+kinghilal@users.noreply.github.com>
2019-01-29 16:13:33 +0300

    Update probot.js

--- probot.js
+++ probot.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
 const Canvas = require("canvas"); //npm i canvas
-const prefix = "+" // برفكس
+const prefix = "+" 
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 
@@ -824,4 +824,4 @@ message.channel.stopTyping()
 })
 }
 });
-client.login("NTM5NTUwODcxNzEzODczOTYx.DzD_tQ.tCWXlIQvjmRk1HF2swKW8xIla4I");  // توكن حقق
+client.login("NTM5NTUwODcxNzEzODczOTYx.DzD_tQ.tCWXlIQvjmRk1HF2swKW8xIla4I");
